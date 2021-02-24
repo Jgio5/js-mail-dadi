1fc6b6c034c0e5a443f3100bd3cbeb904624db2c
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-02-24 01:08:49 +0100

    1 su 2 - email

---
+++ Mail/js/main.js
@@ -0,0 +1,32 @@
+var emailUtente;
+
+var emailDataBase = ["giorgio@hotmail.it", "luca@live.com", "bbsognidoro@hotmail.it", "boolean@info.it", "lucia@libero.net"]
+
+emailUtente = prompt("Inserisci la tua Email");
+console.log(emailUtente);
+document.getElementById('emailx').innerHTML = emailUtente;
+
+if (emailUtente == emailDataBase[0]) {
+  console.log("Email trovata nel database");
+  document.getElementById('email').innerHTML =  "Email trovata nel database";
+}
+else if (emailUtente == emailDataBase[1]) {
+  console.log("Email trovata nel database");
+  document.getElementById('email').innerHTML =  "Email trovata nel database";
+}
+else if (emailUtente == emailDataBase[2]) {
+  console.log("Email trovata nel database");
+  document.getElementById('email').innerHTML =  "Email trovata nel database";
+}
+else if (emailUtente == emailDataBase[3]) {
+  console.log("Email trovata nel database");
+  document.getElementById('email').innerHTML =  "Email trovata nel database";
+}
+else if (emailUtente == emailDataBase[4]) {
+  console.log("Email trovata nel database");
+  document.getElementById('email').innerHTML =  "Email trovata nel database";
+}
+else {
+  console.log("Email non trovata nel database");
+  document.getElementById('email').innerHTML =  "Email non trovata nel database"
+}
