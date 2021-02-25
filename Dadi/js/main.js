702b26e9c284aa42f035c89fa33e21f47b176729
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-02-25 17:12:55 +0100

    Esercizio Completato

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -1,23 +1,44 @@
-var tiri = prompt("inserisci quanti tiri fare");
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+var lancio = prompt("Inserisci quanti lanci fare");
+console.log(lancio);
 
-var numeroDatoPc, numeroDatoUtente;
+var totaleLanciUtente = 0;
+var totaleLanciPc = 0;
 
-
-for (var i = 0; i < tiri; i++) {
-  numeroDatoUtente = Math.floor(Math.random() * 6) + 1;
-
-  numeroDatoPc = Math.floor(Math.random() * 6) + 1;
-
-  console.log(numeroDatoPc);
-  console.log(numeroDatoUtente);
+// Somma dei dati dell'utente
+for (var i = 0; i < lancio; i++ ) {
+  var numeroDatoUtente = Math.floor(Math.random() * 6) + 1;
+  console.log("numero dado utente: " + numeroDatoUtente);
+  // somma numero dadi utente
+  totaleLanciUtente += numeroDatoUtente
 }
 
-if (numeroDatoPc > numeroDatoUtente) {
-  console.log ("Il vincitore è il PC con " + numeroDatoPc + " punti.");
+// Somma dei dati del pc
+for (var i = 0; i < lancio; i++ ) {
+  var numeroDatoPc = Math.floor(Math.random() * 6) + 1;
+  console.log("numero dado PC: " + numeroDatoPc);
+  // somma numero dadi pc
+  totaleLanciPc += numeroDatoPc
 }
-else if (numeroDatoPc < numeroDatoUtente){
-  console.log ("Il vincitore è l'utente " + numeroDatoUtente + " punti.");
+
+// proviamo se la somma dei punteggi funziona
+console.log ("Punteggio totale pc: " + totaleLanciPc);
+console.log ("Punteggio totale utente: " + totaleLanciUtente);
+
+//  troviamo il vincitore
+if (totaleLanciPc > totaleLanciUtente) {
+  console.log ("vince il pc con: " + totaleLanciPc);
+  document.getElementById('vincitore').innerHTML = "Il PC con " + totaleLanciPc + " punti!";
+  // alert("Il vincitore è il PC con " + totaleLanciPc + " punti.");
+}
+else if (totaleLanciPc < totaleLanciUtente) {
+  console.log ("vince l'utente con: " + totaleLanciUtente);
+  document.getElementById('vincitore').innerHTML = "L'utente con " + totaleLanciUtente + " punti!";
+  // alert("Il vincitore è l'utente con " + totaleLanciUtente + " punti.");
 }
 else {
-  console.log ("Risultato uguale");
+  console.log ("Risultato uguale: " + totaleLanciUtente + " " + totaleLanciPc);
+  document.getElementById('vincitore').innerHTML = "Pareggio con " + totaleLanciUtente + " punti per l'utente e il PC";
+  // alert("Pareggio");
 }
